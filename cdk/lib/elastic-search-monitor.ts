afa435072918c1ec6f65d4d9ef26174e0cdcdb84
Jacob Winch <jacob.winch@guardian.co.uk>
2022-04-20 15:32:38 +0100

    Migrate alarms from separate template into cdk

--- cdk/lib/elastic-search-monitor.ts
+++ cdk/lib/elastic-search-monitor.ts
@@ -1,13 +1,19 @@
+import { ComparisonOperator, Metric } from "@aws-cdk/aws-cloudwatch";
 import { SecurityGroup } from "@aws-cdk/aws-ec2";
 import { Schedule } from "@aws-cdk/aws-events";
 import { PolicyStatement } from "@aws-cdk/aws-iam";
 import { Runtime } from "@aws-cdk/aws-lambda";
+import { EmailSubscription } from "@aws-cdk/aws-sns-subscriptions";
 import type { App } from "@aws-cdk/core";
 import { CfnParameter, Duration } from "@aws-cdk/core";
 import { GuScheduledLambda } from "@guardian/cdk";
+import { GuAlarm } from "@guardian/cdk/lib/constructs/cloudwatch";
 import type { GuStackProps } from "@guardian/cdk/lib/constructs/core";
 import { GuStack } from "@guardian/cdk/lib/constructs/core";
 import { GuVpc } from "@guardian/cdk/lib/constructs/ec2";
+import { GuSnsTopic } from "@guardian/cdk/lib/constructs/sns";
+
+const app = "elastic-search-monitor";
 
 export class ElasticSearchMonitor extends GuStack {
   constructor(scope: App, id: string, props: GuStackProps) {
@@ -43,7 +49,7 @@ export class ElasticSearchMonitor extends GuStack {
       pushMetricsPolicy,
     ];
     const scheduledLambda = new GuScheduledLambda(this, "ScheduledLambda", {
-      app: "elastic-search-monitor",
+      app,
       fileName: "elastic-search-monitor.jar",
       environment: {
         TAG_QUERY_APP: "elk-es-master",
@@ -70,5 +76,98 @@ export class ElasticSearchMonitor extends GuStack {
       ],
     });
     additionalPolicies.map((policy) => scheduledLambda.addToRolePolicy(policy));
+
+    const topicForElasticsearchAlerts = new GuSnsTopic(this, "AlertChannel", {
+      displayName: `Elastic search alert channel for ${this.stage}`,
+      topicName: `Elastic-Search-Alerts-${this.stage}`,
+      existingLogicalId: {
+        logicalId: "AlertChannel",
+        reason: "Migrated from CloudFormation",
+      },
+    });
+
+    topicForElasticsearchAlerts.addSubscription(
+      new EmailSubscription("devx.sec.ops@guardian.co.uk")
+    );
+
+    const clusterName = "elk";
+
+    const metric = (metricName: string) => {
+      return new Metric({
+        metricName,
+        namespace: `${this.stack}/${clusterName}`,
+        dimensionsMap: {
+          Cluster: clusterName,
+        },
+      });
+    };
+
+    const commonAlarmProps = {
+      app,
+      snsTopicName: topicForElasticsearchAlerts.topicName,
+      period: Duration.minutes(1),
+    };
+
+    const lessThanAlarmProps = {
+      ...commonAlarmProps,
+      comparisonOperator: ComparisonOperator.LESS_THAN_THRESHOLD,
+      statistic: "Minimum",
+    };
+
+    const greaterThanAlarmProps = {
+      ...commonAlarmProps,
+      comparisonOperator: ComparisonOperator.GREATER_THAN_THRESHOLD,
+      statistic: "Maximum",
+    };
+
+    new GuAlarm(this, "DataNodeCountAlarm", {
+      ...lessThanAlarmProps,
+      alarmDescription: `Unexpected count of data nodes in ${this.stage}`,
+      evaluationPeriods: 2,
+      metric: metric("NumberOfDataNodes"),
+      threshold: 3,
+    });
+
+    new GuAlarm(this, "MasterNodeCountAlarm", {
+      ...lessThanAlarmProps,
+      alarmDescription: `Unexpected count of master nodes in ${this.stage}`,
+      evaluationPeriods: 2,
+      metric: metric("NumberOfRespondingMastersNodes"),
+      threshold: 3,
+    });
+
+    new GuAlarm(this, "ClusterStatusAlarm", {
+      ...greaterThanAlarmProps,
+      alarmDescription: `Unexpected cluster status in ${this.stage}`,
+      evaluationPeriods: 15,
+      metric: metric("Status"),
+      // Green = 0, Yellow = 1, Red = 2
+      threshold: 0,
+    });
+
+    new GuAlarm(this, "RedClusterStatusAlarm", {
+      ...greaterThanAlarmProps,
+      alarmDescription: `Red cluster status in ${this.stage}`,
+      evaluationPeriods: 1,
+      metric: metric("Status"),
+      // Green = 0, Yellow = 1, Red = 2
+      threshold: 1,
+    });
+
+    new GuAlarm(this, "DataNodeJvmHeapUsageAlarm", {
+      ...greaterThanAlarmProps,
+      alarmDescription: `A data node is using too much of its heap in ${this.stage}`,
+      evaluationPeriods: 2,
+      metric: metric("MaxJvmHeapUsage"),
+      threshold: 85,
+    });
+
+    new GuAlarm(this, "DataNodeLowStorageAlarm", {
+      ...lessThanAlarmProps,
+      alarmDescription: `A data node is running low on storage space in ${this.stage}`,
+      evaluationPeriods: 2,
+      metric: metric("MinFreeDiskSpace"),
+      threshold: 500000000000,
+    });
   }
 }
