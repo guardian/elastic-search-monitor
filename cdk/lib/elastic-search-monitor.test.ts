09b6a22403ec7ed39a70836a8ed0ba129422a7cd
Jorge Azevedo <jorge.azevedo@guardian.co.uk>
2026-02-23 17:07:34 +0000

    Format CDK TypeScript code with prettier

--- cdk/lib/elastic-search-monitor.test.ts
+++ cdk/lib/elastic-search-monitor.test.ts
@@ -1,14 +1,14 @@
-import { App } from "aws-cdk-lib";
-import { Template } from "aws-cdk-lib/assertions";
-import { ElasticSearchMonitor } from "./elastic-search-monitor";
+import { App } from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { ElasticSearchMonitor } from './elastic-search-monitor';
 
-describe("The ElasticSearchMonitor stack", () => {
-  it("matches the snapshot", () => {
-    const app = new App();
-    const stack = new ElasticSearchMonitor(app, "ElasticSearchMonitor", {
-      stack: "deploy",
-      stage: "TEST",
-    });
-    expect(Template.fromStack(stack)).toMatchSnapshot();
-  });
+describe('The ElasticSearchMonitor stack', () => {
+	it('matches the snapshot', () => {
+		const app = new App();
+		const stack = new ElasticSearchMonitor(app, 'ElasticSearchMonitor', {
+			stack: 'deploy',
+			stage: 'TEST',
+		});
+		expect(Template.fromStack(stack)).toMatchSnapshot();
+	});
 });
